49f83acafd7ca311eba62b905de72ed3aa968954
Mirko Kugelmeier <mirko.kugelmeier@icecube.wisc.edu>
2024-06-07 08:31:09 -0100

    Split preprocessing into seperate file

--- main.py
+++ main.py
@@ -3,55 +3,23 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+import matplotlib as mpl
 import matplotlib.backends.backend_pdf
 
 from log_reader import LogReader
+from preprocess import preprocess
 
 if __name__ == "__main__":
-    logfile = "C:\\Users\\WIPAC\\NERC\\BAS BigRAID - Documents\\Season Reports\\Data from control box\\logs\\Data Log\\2022 06 24 0000 (Tagname).DAT"
+    logfile = "C:\\Users\\WIPAC\\NERC\\BAS BigRAID - Documents\\Season Reports\\2024\\DataLog\\2024 05 30 0000 BigRAID (Tagname).DAT"
+    #logfile = r"C:\Users\WIPAC\NERC\BAS BigRAID - Documents\Season Reports\Data from control box\logs\Data Log\2022 07 07 0000 (Tagname).DAT"
+    #logfile = "D:\\DataLog\\2024 06 01 0000 BigRAID (Tagname).DAT"
     logfile = Path(logfile)
 
     reader = LogReader(tagfile=logfile)
     df = reader.as_df()
 
-    # Filter the data slightly, since the drill can sometimes record spurious bad values
-    df = df.rolling(3).median()
-
-    # Do some cleanup
-    df[df["[PLC]DRILLACTIVECURRENT"].abs() > 1e10] = np.nan
-    df.loc[df["[PLC]WIRESPOOLEDOUT"].abs() > 200, "[PLC]WIRESPOOLEDOUT"] = np.nan
-    df["[PLC]WIRESPOOLEDOUT"] = df["[PLC]WIRESPOOLEDOUT"].ffill()
-    df[df["[PLC]DRILLFEEDBACKVEL"].abs() > 100] = np.nan
-
-    df = df.dropna()
-
-    # Mark drill runs
-    spooled_out = df["[PLC]WIRESPOOLEDOUT"] > 1.5
-    df['run'] = 1.0
-    df.loc[spooled_out.gt(spooled_out.shift()), 'run'] = 0
-    run_ids = df['run'].ne(df['run'].shift()).cumsum() - 1
-    in_run = df["run"] != 0.0
-    df.loc[in_run, "run"] = run_ids.loc[in_run] / 2
-    df.loc[df['run'] == 0.0, 'run'] = df['run'].shift(-1)
-    df[df['run'] == 0.0] = 1.0
-
-    # Add a column that indicates if active cutting of new ice was happening. Kind of a guess based on other parameters
-    df["cutting"] = 0
-    df.loc[(df["[PLC]DRILLACTIVECURRENT"] > 0.5) & (df["run"] > 0) & (df["[PLC]WIRESPOOLEDOUT"] > 1) & (df["[PLC]CABLESPEED"].abs() < 1), "cutting"] = 1
-    # Filter out cutting state where the payout is not relatively close to the maximum
-    group_max_out = df["[PLC]WIRESPOOLEDOUT"].groupby(df["run"]).transform("max")
-    df.loc[((group_max_out / df["[PLC]WIRESPOOLEDOUT"]).fillna(0) > 10), "cutting"] = 0
-
-    # Calculate the (running) cut depth drilled per run. For each run, this starts at 0 when drilling begins at
-    # the bottom of the hole, and increases until drilling stops
-    df["cut_depth"] = df[df["cutting"] == 1]["[PLC]WIRESPOOLEDOUT"].groupby(df["run"]).transform("first")
-    df["cut_depth"] =  df["[PLC]WIRESPOOLEDOUT"] - df["cut_depth"]
-    df["cut_depth"] = df['cut_depth'].groupby(df["run"]).transform("cummax")
-    df["cut_depth"] = df["cut_depth"].fillna(0)
-
-    # Calculate the weight-on-bit
-    # Assumes cable weight of 159g/m and ice density of 0.91g/mL
-    df["weight_on_bit"] = (-1 * df["[PLC]CABLETENSION"] + df["[PLC]WIRESPOOLEDOUT"] * 0.159 + 280 + ((0.285/2)**2 * np.pi * df['cut_depth'] * 910))
+    # Add additional calculated columns to the data
+    df = preprocess(df)
 
     df[df["cutting"] == 1].plot.scatter(x="cut_depth", y="[PLC]DRILLACTIVECURRENT", c="run")
     plt.title("Motor Current vs Running Cut Depth")
@@ -94,16 +62,16 @@ if __name__ == "__main__":
     ax.set_ylabel("Time [s]")
     ax.set_xlabel("Run")
 
-    fig, axes = plt.subplots(1, 2)
-    for i, k in enumerate(["[PLC]IMUPITCH", "[PLC]IMUROLL"]):
-        d = df[df[k].abs() < 2]
-        # subtract the mean when the drill is hanging freely above the hole
-        zero_offset = df[(df["[PLC]WIRESPOOLEDOUT"] < 1) & (df["[PLC]CABLESPEED"].abs() < 0.1) & (df["[PLC]DRILLFEEDBACKVEL"].abs() < 0.1)][k].mean()
-        axes[i].hist2d(d[k] - zero_offset, d["[PLC]WIRESPOOLEDOUT"], bins=35)
-        axes[i].set_title(k)
-    fig.suptitle("Angle per Depth")
-    fig.supylabel("Wire spooled out [m]")
-    fig.supxlabel("Angle [deg]")
+    # fig, axes = plt.subplots(1, 2)
+    # for i, k in enumerate(["[PLC]IMUPITCH", "[PLC]IMUROLL"]):
+    #     d = df[(df[k].abs() < 2) & (df["[PLC]WIRESPOOLEDOUT"] > 2)]
+    #     # subtract the mean when the drill is hanging freely above the hole
+    #     zero_offset = df[(df["[PLC]WIRESPOOLEDOUT"] < 1) & (df["[PLC]CABLESPEED"].abs() < 0.1) & (df["[PLC]DRILLFEEDBACKVEL"].abs() < 0.1)][k].mean()
+    #     axes[i].hist2d(d[k] - zero_offset, d["[PLC]WIRESPOOLEDOUT"], bins=35)
+    #     axes[i].set_title(k)
+    # fig.suptitle("Angle per Depth")
+    # fig.supylabel("Wire spooled out [m]")
+    # fig.supxlabel("Angle [deg]")
 
     df.plot.scatter(y="[PLC]WIRESPOOLEDOUT", x="[PLC]AUTODOWNSTOPDEPTH", c="run")
     plt.title("Stop Depth vs Depth")
---
+++ preprocess.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def preprocess(df):
+    # Filter the data slightly, since the drill can sometimes record spurious bad values
+    df = df.rolling(3).median()
+
+    # Do some cleanup
+    df[df["[PLC]DRILLACTIVECURRENT"].abs() > 1e10] = np.nan
+    df.loc[df["[PLC]WIRESPOOLEDOUT"].abs() > 200, "[PLC]WIRESPOOLEDOUT"] = np.nan
+    df["[PLC]WIRESPOOLEDOUT"] = df["[PLC]WIRESPOOLEDOUT"].ffill()
+    df[df["[PLC]DRILLFEEDBACKVEL"].abs() > 100] = np.nan
+
+    df = df.dropna()
+
+    # Mark drill runs
+    spooled_out = df["[PLC]WIRESPOOLEDOUT"] > 1.5
+    df['run'] = 1.0
+    df.loc[spooled_out.gt(spooled_out.shift()), 'run'] = 0
+    run_ids = df['run'].ne(df['run'].shift()).cumsum() - 1
+    in_run = df["run"] != 0.0
+    df.loc[in_run, "run"] = run_ids.loc[in_run] / 2
+    df.loc[df['run'] == 0.0, 'run'] = df['run'].shift(-1)
+    df[df['run'] == 0.0] = 1.0
+
+    # Add a column that indicates if active cutting of new ice was happening. Kind of a guess based on other parameters
+    df["cutting"] = 0
+    df.loc[(df["[PLC]DRILLACTIVECURRENT"] > 0.5) & (df["run"] > 0) & (df["[PLC]WIRESPOOLEDOUT"] > 1) & (df["[PLC]CABLESPEED"].abs() < 1), "cutting"] = 1
+    # Filter out cutting state where the payout is not relatively close to the maximum
+    group_max_out = df["[PLC]WIRESPOOLEDOUT"].groupby(df["run"]).transform("max")
+    df.loc[((group_max_out / df["[PLC]WIRESPOOLEDOUT"]).fillna(0) > 10), "cutting"] = 0
+
+    # Calculate the (running) cut depth drilled per run. For each run, this starts at 0 when drilling begins at
+    # the bottom of the hole, and increases until drilling stops
+    df["cut_depth"] = df[df["cutting"] == 1]["[PLC]WIRESPOOLEDOUT"].groupby(df["run"]).transform("first")
+    df["cut_depth"] =  df["[PLC]WIRESPOOLEDOUT"] - df["cut_depth"]
+    df["cut_depth"] = df['cut_depth'].groupby(df["run"]).transform("cummax")
+    df["cut_depth"] = df["cut_depth"].fillna(0)
+
+    # Calculate the weight-on-bit
+    # Assumes cable weight of 159g/m and ice density of 0.91g/mL
+    df["weight_on_bit"] = (-1 * df["[PLC]CABLETENSION"] + df["[PLC]WIRESPOOLEDOUT"] * 0.159 + 280 + ((0.285/2)**2 * np.pi * df['cut_depth'] * 910))
+
+    return df
