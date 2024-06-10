54944ac9608f00ca8512d9012ff318d75cae5bbe
Mirko Kugelmeier <mirko.kugelmeier@icecube.wisc.edu>
2024-06-10 13:45:58 -0100

    Update pitch/roll plots

--- main.py
+++ main.py
@@ -65,16 +65,47 @@ if __name__ == "__main__":
     ax.set_ylabel("Time [s]")
     ax.set_xlabel("Run")
 
-    # fig, axes = plt.subplots(1, 2)
-    # for i, k in enumerate(["[PLC]IMUPITCH", "[PLC]IMUROLL"]):
-    #     d = df[(df[k].abs() < 2) & (df["[PLC]WIRESPOOLEDOUT"] > 2)]
-    #     # subtract the mean when the drill is hanging freely above the hole
-    #     zero_offset = df[(df["[PLC]WIRESPOOLEDOUT"] < 1) & (df["[PLC]CABLESPEED"].abs() < 0.1) & (df["[PLC]DRILLFEEDBACKVEL"].abs() < 0.1)][k].mean()
-    #     axes[i].hist2d(d[k] - zero_offset, d["[PLC]WIRESPOOLEDOUT"], bins=35)
-    #     axes[i].set_title(k)
-    # fig.suptitle("Angle per Depth")
-    # fig.supylabel("Wire spooled out [m]")
-    # fig.supxlabel("Angle [deg]")
+    fig, axes = plt.subplots(1, 2)
+    display_max = 0
+    for i, k in enumerate(["[PLC]IMUPITCH", "[PLC]IMUROLL"]):
+        d = df[(df[k].abs() < 2) & (df["[PLC]WIRESPOOLEDOUT"] > 2)]
+        # subtract the mean when the drill is hanging freely above the hole
+        zero_offset = df[(df["[PLC]WIRESPOOLEDOUT"] < 1) & (df["[PLC]CABLESPEED"].abs() < 0.1) & (df["[PLC]DRILLFEEDBACKVEL"].abs() < 0.1)][k].mean()
+
+        vals = d[k] - zero_offset
+        display_max = max(vals.abs().quantile(0.99), display_max)
+        depth_max = df['[PLC]WIRESPOOLEDOUT'].max()
+        H, xedges, yedges = np.histogram2d(d["[PLC]WIRESPOOLEDOUT"], d[k] - zero_offset, bins=[int(depth_max/2), 50], range=[[0, depth_max], [-2, 2]])
+        H_norm_rows = H / H.max(axis=1, keepdims=True)
+        H_norm_rows = np.nan_to_num(H_norm_rows)
+        axes[i].pcolormesh(yedges, xedges, H_norm_rows)
+        axes[i].set_title(k)
+    for ax in axes:
+        ax.set_xlim(-display_max, display_max)
+    fig.suptitle("Angle per Depth, normalized using IMU Yaw")
+    fig.supylabel("Wire spooled out [m]")
+    fig.supxlabel("Angle [deg]")
+
+    fig, axes = plt.subplots(1, 2)
+    display_max = 0
+    for i, k in enumerate(["hole_pitch", "hole_roll"]):
+        d = df[(df[k].abs() < 2) & (df["[PLC]WIRESPOOLEDOUT"] > 2)]
+        # subtract the mean when the drill is hanging freely above the hole
+        zero_offset = df[(df["[PLC]WIRESPOOLEDOUT"] < 1) & (df["[PLC]CABLESPEED"].abs() < 0.1) & (df["[PLC]DRILLFEEDBACKVEL"].abs() < 0.1)][k].mean()
+
+        vals = d[k] - zero_offset
+        display_max = max(vals.abs().quantile(0.99), display_max)
+        depth_max = df['[PLC]WIRESPOOLEDOUT'].max()
+        H, xedges, yedges = np.histogram2d(d["[PLC]WIRESPOOLEDOUT"], d[k] - zero_offset, bins=[int(depth_max/2), 50], range=[[0, depth_max], [-2, 2]])
+        H_norm_rows = H / H.max(axis=1, keepdims=True)
+        H_norm_rows = np.nan_to_num(H_norm_rows)
+        axes[i].pcolormesh(yedges, xedges, H_norm_rows)
+        axes[i].set_title(k)
+    for ax in axes:
+        ax.set_xlim(-display_max, display_max)
+    fig.suptitle("Angle per Depth, normalized using IMU Yaw")
+    fig.supylabel("Wire spooled out [m]")
+    fig.supxlabel("Angle [deg]")
 
     df.plot.scatter(y="[PLC]WIRESPOOLEDOUT", x="[PLC]AUTODOWNSTOPDEPTH", c="run")
     plt.title("Stop Depth vs Depth")
