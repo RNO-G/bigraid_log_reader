10ec367a7787ff4b7ae1339c2a88d78a0fa3770e
Mirko Kugelmeier <mirko.kugelmeier@icecube.wisc.edu>
2025-06-02 09:35:04 -0100

    Filter out large lock depth values

--- preprocess.py
+++ preprocess.py
@@ -8,6 +8,7 @@ def preprocess(df, run_depth_threshold=1.5):
     df[df["[PLC]DRILLACTIVECURRENT"].abs() > 1e10] = np.nan
     df.loc[df["[PLC]WIRESPOOLEDOUT"].abs() > 200, "[PLC]WIRESPOOLEDOUT"] = np.nan
     df["[PLC]WIRESPOOLEDOUT"] = df["[PLC]WIRESPOOLEDOUT"].ffill()
+    df.loc[df["[PLC]AUTODOWNSTOPDEPTH"].abs() > 200, "[PLC]AUTODOWNSTOPDEPTH"] = np.nan
     df[df["[PLC]DRILLFEEDBACKVEL"].abs() > 100] = np.nan
     df[df['[PLC]DRILLTEMP'] < -100 ] = np.nan
     df = df.dropna()
